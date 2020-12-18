054f51a5a0d0ac074e7735fbd45aa4cc5d4d1b12
zsarge <zackarywsargent@gmail.com>
2020-12-18 13:21:03 -0500

    Add messy scripts to look though text

--- AnalyzingChat/analyze_chat.py
+++ AnalyzingChat/analyze_chat.py
@@ -1,15 +1,82 @@
-from secret import path 
-
+# path should be a .json file produced by https://github.com/Tyrrrz/DiscordChatExporter
+from secret import path
 import json
 
 print(path)
 
+with open(path) as f:
+      content = json.load(f)
 
-#  with open(path) as f:
-      #  content = json.load(f)
+message_lengths = []
 
-#  message_lengths = []
+for chat in content["messages"]:
+	message_lengths.append(len(chat["content"]))
 
-#  for i in content["messages"]:
+def get_message_with_length(n):
+	for chat in content["messages"]:
+		if len(chat["content"]) == n:
+			return chat["content"]
 
+def get_messages_with_length(n):
+	messages = []
+	for chat in content["messages"]:
+		if len(chat["content"]) == n:
+			messages.append(chat["content"])
+	return messages
 
+def get_author_of_message(message_content):
+    authors = []
+    for chat in content["messages"]:
+        if chat["content"] == message_content:
+            authors.append(chat["author"]["name"])
+    if len(authors) > 1:
+        print(f"{len(authors)} messages found with {message_content}")
+        criminals = {}
+        for i in authors:
+            try:
+                criminals[i] += 1
+            except:
+                criminals[i] = 1
+        print(criminals)
+
+    return authors[0]
+
+def get_author_of_message_with(message_content):
+    authors = []
+    for chat in content["messages"]:
+        if message_content in chat["content"]:
+            authors.append(chat["author"]["name"])
+    if len(authors) > 1:
+        print(f"{len(authors)} messages found with {message_content}")
+        criminals = {}
+        for i in authors:
+            try:
+                criminals[i] += 1
+            except:
+                criminals[i] = 1
+        print(criminals)
+
+    return authors[0]
+
+def average(a):
+    n = len(a)
+    # Find sum of array element
+    sum = 0
+    for i in range(n):
+        sum += a[i]
+    return sum/n;
+
+print(f"average length - {average(message_lengths)}")
+print(f"number of messages - {len(message_lengths)}")
+
+max_len = max(message_lengths)
+longest_message = get_message_with_length(max_len)
+print(f"longest message = {longest_message}")
+print(get_author_of_message(longest_message))
+
+#  quirky_bois = get_messages_with_length(69)
+#  for i in quirky_bois:
+    #  print(f"quirkiest messages = {i} \n\t^^ by: {get_author_of_message(i)}")
+
+#  print(f"owo -- {get_author_of_message('owo')}")
+print(f"owo -- {get_author_of_message_with('owo')}")
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # random-python
 random python scripts that I want version control for, but I don't want a full repository for
+
+AnalyzingChat is stuff related to analyzing the results from https://github.com/Tyrrrz/DiscordChatExporter
