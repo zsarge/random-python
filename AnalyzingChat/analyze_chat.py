698b05825bbc557ce4f4ad02148372b589e49382
zsarge <zackarywsargent@gmail.com>
2020-12-18 12:39:32 -0500

    Initial python

---
+++ AnalyzingChat/analyze_chat.py
@@ -0,0 +1,15 @@
+from secret import path 
+
+import json
+
+print(path)
+
+
+#  with open(path) as f:
+      #  content = json.load(f)
+
+#  message_lengths = []
+
+#  for i in content["messages"]:
+
+
